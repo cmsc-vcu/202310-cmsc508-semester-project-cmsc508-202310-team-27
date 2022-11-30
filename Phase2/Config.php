b97f868658b9b5cb8e763855e10daaa04f9cf1bd
Lazaro Camero <cameroruizla@vcu.edu>
2022-11-30 18:37:57 -0500

    .

---
+++ Phase2/Config.php
@@ -0,0 +1,13 @@
+<!-- Connect to the database -->
+
+<?php
+    define('USER', 'root');
+    define('PASSWORD', '');
+    define('HOST', 'localhost');
+    define('DATABASE', 'test');
+    try {
+        $connection = new PDO("mysql:host=".HOST.";dbname=".DATABASE, USER, PASSWORD);
+    } catch (PDOException $e) {
+        exit("Error: " . $e->getMessage());
+    }
+?>
---
+++ Phase2/Login.php
@@ -0,0 +1,29 @@
+<!-- 
+    Logging users in.
+    This checks the information in the 
+    database to see if the username and password combination entered into the form 
+    is correct.
+-->
+
+<?php
+    session_start();
+    include('config.php');
+    if (isset($_POST['login'])) {
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+        $query = $connection->prepare("SELECT * FROM users WHERE username=:username");
+        $query->bindParam("username", $username, PDO::PARAM_STR);
+        $query->execute();
+        $result = $query->fetch(PDO::FETCH_ASSOC);
+        if (!$result) {
+            echo '<p class="error">Username password combination is wrong!</p>';
+        } else {
+            if (password_verify($password, $result['password'])) {
+                $_SESSION['user_id'] = $result['id'];
+                echo '<p class="success">Congratulations, you are logged in!</p>';
+            } else {
+                echo '<p class="error">Username password combination is wrong!</p>';
+            }
+        }
+    }
+?>
