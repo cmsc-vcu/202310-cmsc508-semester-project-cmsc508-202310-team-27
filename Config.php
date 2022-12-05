ed54397278f4e91faac23c28a6226ddb7f1c07cb
Mir Ali <99275887+Mir-Ali1@users.noreply.github.com>
2022-12-05 12:37:38 -0500

    Add files via upload

---
+++ Config.php
@@ -0,0 +1,13 @@
+<!-- Connect to the database -->
+
+<?php
+    define('USER', 'alimm3');
+    define('PASSWORD', 'php server password');
+    define('HOST', 'cmsc508.com');
+    define('DATABASE', '202310_users_alimm3');
+    try {
+        $connection = new PDO("mysql:host=".HOST.";dbname=".DATABASE, USER, PASSWORD);
+    } catch (PDOException $e) {
+        exit("Error: " . $e->getMessage());
+    }
+?>
