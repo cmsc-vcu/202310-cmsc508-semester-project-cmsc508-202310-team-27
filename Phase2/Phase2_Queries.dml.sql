f2e558502b8a8c20d98b7c2ad41d45d1736056b5
Lazaro Camero <cameroruizla@vcu.edu>
2022-12-09 16:13:54 -0500

    Changed

--- Phase2/Phase2_Queries.dml.sql
+++ Phase2/Phase2_Queries.dml.sql
@@ -83,17 +83,17 @@ ORDER BY  Medical_Check_up_date;
 SELECT *
 FROM Disease
 WHERE 1=1
-        AND Disease_name = 'Flu';
+        AND Disease_name = 'Diphtheria';
 
 -- task 8
 -- How many Asian people have the mad cow disease?
 
-SELECT count(Patient_ethnicity) AS 'Ethnicity', Disease_name
+SELECT COUNT(Patient_ethnicity) AS 'Asians', Disease_name
 FROM Patient a
 JOIN Disease b
-    ON (Patient_ID = Disease_Patient_ID)
+    ON (Patient_ID = Disease_ID)
 WHERE 1=1
-        AND Disease_name LIKE 'Mad Cow'
+        AND Disease_name LIKE 'COVID'
 GROUP BY  disease_name;
 
 -- task 9
@@ -113,10 +113,11 @@ WHERE 1=1
 
 SELECT CONCAT(Patient_first_name,' ', Patient_last_name) AS "Name",
         Medical_Check_up_date AS "Last Checkup"
-FROM Patient a
-JOIN Medical_Check_up b
-    ON (Patient_ID = Medical_Check_up_Patient_ID)
+FROM Patients_Checkups a
+JOIN Patient b ON (a.Patient_ID = b.Patient_ID)
+JOIN Medical_Check_up c ON (a.Medical_Check_up_ID = c.Medical_Check_up_ID)
 ORDER BY  Patient_last_name, Medical_Check_up_date;
+
 
 -- task 11
 -- What is the socioeconomic status of every patient?
@@ -129,34 +130,29 @@ FROM Patient;
 -- List every patient's medical history?
 
 SELECT
-CONCAT(Patient_first_name,' ', Patient_last_name) AS "Name",
-Medical_Check_up_documentation
-FROM Patient a 
-JOIN Medical_Check_up b ON (Patient_ID = Medical_Check_up_Patient_ID)
+CONCAT(Patient_first_name,' ', Patient_last_name) AS "Name", Medical_Check_up_documentation
+FROM Patients_Checkups a 
+JOIN Medical_Check_up b ON (a.Medical_Check_up_ID = b.Medical_Check_up_ID)
+JOIN Patient c ON (a.Patient_ID = c.Patient_ID)
 ORDER BY Patient_last_name, Medical_Check_up_documentation;
 
 -- task 13
--- Write a single query that shows the reason for the patients medical checkups 
--- Display first name and last name of the patients and order by medical checkups
+-- A patient has changed her last name. She just got married
+-- Update an entry in the Patient's table
+-- What changes have been made to the patient's table?
 
-SELECT CONCAT(Patient_first_name,' ', Patient_last_name)
-        AS "Name", Medical_Check_up_reason
-FROM Patient a
-LEFT JOIN Medical_Check_up b
-    ON (Patient_ID = Medical_Check_up_Patient_ID)
-ORDER BY  Medical_Check_up_reason;
-
--- task 14
--- If a patient gets married and changes her last name,
--- How can we manage this in the database? Write a query to update 
--- the a patient's name.
-
-
-UPDATE Patient
-SET Patient_last_name = 'Poff'
+UPDATE Patient 
+SET Patient_last_name = "Poff"
 WHERE Patient_gender = 'Female' AND Patient_age = 23;
 
-SELECT * FROM Trigger_insert
+SELECT * FROM Trigger_Patient;
+
+-- task 14
+-- Display a list with of all the doctors' informations
+
+SELECT * FROM Doctor;
+
+
 -- task 15
 -- What is the environmental status of Canada?
 
@@ -167,7 +163,7 @@ WHERE 1=1
         AND Region_country_short_name = "Canada"
 ORDER BY Region_country_short_name, Region_environment;
 
--- task 16
+-- task 
 -- How many people got their medical checkups in the year 2020?
 
 SELECT COUNT(Patient_first_name) AS "People",
@@ -184,16 +180,16 @@ ORDER BY  2;
 -- Their origins come from places outside of the United States, therefore
 -- how many patients from Latin America & Caribbean have a disease?
 
-SELECT COUNT(Patient_first_name) AS "Patient",
+SELECT CONCAT(Patient_first_name, ' ', Patient_last_name) AS "Patient",
          Region,
          Disease_name
 FROM Patient a
 JOIN Region b
     ON (Patient_ID = Region_Patient_ID)
 JOIN Disease c
-    ON (Patient_ID = Disease_Patient_ID)
+    ON (c.Disease_ID = b.Region_Disease_ID)
 WHERE Region = 'Latin America & Caribbean'
-GROUP BY  Region, Disease_name;
+GROUP BY  Region, Disease_name,Patient_first_name, Patient_last_name;
 
 -- task 18
 -- Some of the patients come for medical checkups, however, in the meanwhile
